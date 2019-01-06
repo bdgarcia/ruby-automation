76537cce9a5330fdd8b5dcb83befd20a6af664fe
Bruno Garcia <bgarcia@everfi.com>
2019-01-06 16:01:05 -0300

    added GET requests test sample

---
+++ test/google/request.rb
@@ -0,0 +1,20 @@
+require "net/https"
+require "uri"
+
+describe 'GET request', typr: :feature do
+    context 'When the request tries to get a teapot' do
+      it 'shows 418 teapot code' do
+        uri = URI.parse("https://www.google.com/teapot")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        
+        puts "Getting status code from GET request to https://www.google.com/teapot"
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+
+        puts "The status code is: #{response.code}"
+
+        expect(response.code).to include("418")
+      end
+    end
+end
